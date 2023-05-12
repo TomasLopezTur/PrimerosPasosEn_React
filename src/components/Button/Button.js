3bb127e6b73f182172a94eba02f2d5021c02aa0d
TomasLopezTur <tomas.lopez.tur@gmail.com>
2023-05-12 19:40:47 -0300

    props

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,5 @@
 import "./Button.css"
 
 export const Button = (props) => {
-    return <button className={props.className}  /* style={{backgroundColor: "red"}} */>{props.text}</button>;
+    return <button className={props.className} onClick={props.handleClick} /* style={{backgroundColor: "red"}} */>{props.text}</button>;
 }
--- src/components/ConditionalRender/index.jsx
+++ src/components/ConditionalRender/index.jsx
@@ -31,7 +31,11 @@ export const ConditionalRender = ()=>{
     return (
         <Fragment>
             <h2 onClick={handleClick}>Renderizado condicional</h2>
-            {/* content(true) */ isLoggedIn ? <p>"Logueado"</p> : <Button text="Condicional" className="btnStyle" handleClick={handleUserLogin} />}
+            {/* content(true) */ isLoggedIn ? <div>
+                <p>"Logueado"</p> 
+                <Button text="Logout" className="btn-green" handleClick={handleUserLogout} />
+            </div> :
+            <Button text="Login" className="btnStyle" handleClick={handleUserLogin} /> }
             <div>
             <input type="text" onChange={handleChange} />
             </div>
